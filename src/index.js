6bb7a2caf2ccf061b9888014e00c7183419cb799
Carlo Diaz <carlodiaz139@gmail.com>
2022-10-19 11:20:33 -0400

    import amplify styles file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
