a0ab6542758d4426ff17ba991afe7a3e9175a64d
Paolo Ihde <pihde001@odu.edu>
2022-10-25 21:40:33 -0400

    added login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Login(props) {
+    return (
+        <>
+        <div className="loginContainer">
+            <h1>Welcome to Art Guardian</h1>
+            <form className="loginForm">
+                <div className="loginFormContent">
+                    <h3 className="loginFormTitle">Sign In</h3>
+                    <div className="formInputs">
+                        <div className="form-group mt-3">
+\                            <input type="text" className="form-control mt-1" placeholder="Enter Email" />
+                        </div>
+                        <div className="form-group mt-3">
+                            <input type="password" className="form-control mt-1" placeholder="Enter Password" />
+                        </div>
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+                    <p>Don't have an account?</p>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="button" className="btn btn-primary">Register</button>
+                    </div>
+                </div>
+            </form>
+        </div></>
+    )
+}
