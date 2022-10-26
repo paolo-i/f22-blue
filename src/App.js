8688cc0559ceee8ed68f4422df5863b46a2691e6
Carlo Diaz <carlodiaz139@gmail.com>
2022-10-26 12:38:28 -0400

    Create pages directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import NavBar from './custom-components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
