d8c12fa7548c8d23839ee3138839c84c8b67464d
Paolo Ihde <pihde001@odu.edu>
2022-10-27 15:45:53 -0400

    modified color scheme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/SignInPage';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/SignInPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />}></Route>
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
