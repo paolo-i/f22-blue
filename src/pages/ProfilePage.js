3fcb7b53d90d2a33362ece7170bd2a35d8b7924a
Arobe022 <arobe022@odu.edu>
2022-11-15 09:44:43 -0500

    Still working on profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Header, Form, Button, Message, Input, Segment, Select } from 'semantic-ui-react';
 import NavBar from '../custom-components/NavBar'
 import { Auth } from 'aws-amplify'; 
+import {Authenticator} from 'aws-amplify';
 import Amplify from 'aws-amplify';
 
 import aws_exports from '../aws-exports';
@@ -20,9 +21,8 @@ class profile extends Component {
         this.state = {
             authState: this.props.authState,
             email:'',
-            given_name:'',
-            middle_name:'',
-            family_name:'',
+            first_name:'',
+            last_name:'',
             birthdate:'',
             gender:'',
             phone_number:'',
@@ -41,16 +41,15 @@ class profile extends Component {
             authData: user,
             authState: 'signedIn',
             
-            nickname: user.attributes.nickname ? user.attributes.nickname : '',
+            
             email:user.attributes.email ? user.attributes.email : '',
-            given_name: user.attributes.given_name ? user.attributes.given_name : '',
-            middle_name: user.attributes.middle_name ? user.attributes.middle_name : '',
-            family_name: user.attributes.family_name ? user.attributes.family_name : '',
+            first_name: user.attributes.given_name ? user.attributes.given_name : '',
+            last_name: user.attributes.family_name ? user.attributes.family_name : '',
             birthdate: user.attributes.birthdate ? user.attributes.birthdate : '',
             gender: user.attributes.gender ? user.attributes.gender : '',
             phone_number: user.attributes.phone_number ? user.attributes.phone_number : '',
             address: user.attributes.address ? user.attributes.address : '',
-            website: user.attributes.website ? user.attributes.website : '',
+           
             
             stateFromStorage: true
         });
@@ -67,16 +66,15 @@ class profile extends Component {
     
     handleSubmit = () => {
         Auth.updateUserAttributes(this.state.authData,{
-            'nickname': this.state.nickname,
+            
             'email':this.state.email,
-            'given_name':this.state.given_name,
-            'middle_name':this.state.middle_name,
-            'family_name':this.state.family_name,
+            'first_name':this.state.given_name,
+            'last_name':this.state.family_name,
             'birthdate':this.state.birthdate,
             'gender':this.state.gender,
             'phone_number':this.state.phone_number,
             'address':this.state.address,
-            'website':this.state.website,
+            
         }).catch(e => { 
             console.log('Error updating user: '); 
             console.log(e); 
@@ -88,9 +86,11 @@ class profile extends Component {
         let loading = true;
         if(this.state.authData) { loading = false; } 
         
-        const {nickname,email,given_name,middle_name,family_name,birthdate,gender,phone_number,address,website} = this.state;
+        const {username,email,first_name,last_name,birthdate,gender,phone_number,address} = this.state;
         
         return(
+            <Authenticator>
+            {({user }) => (
             <><NavBar /><Segment color="blue">
                 <Grid padded>
                     <Grid.Column>
@@ -100,14 +100,13 @@ class profile extends Component {
                         <Form onSubmit={this.handleSubmit} loading={loading}>
 
                             <Form.Group>
-                                <Form.Input name='nickname' value={nickname} label='Display Name' placeholder='Display name' width={6} onChange={this.handleChange} error={false} />
-                                <Form.Input label='Username' value={email} width={10} />
+                            <Form.Input label='email' value={email} width={10} />
                             </Form.Group>
 
                             <Form.Group>
-                                <Form.Input name='given_name' value={given_name} label='First name' placeholder='First Name' width={6} onChange={this.handleChange} error={false} />
-                                <Form.Input name='middle_name' value={middle_name} label='Middle Name' placeholder='Middle Name' width={4} onChange={this.handleChange} error={false} />
-                                <Form.Input name='family_name' value={family_name} label='Last Name' placeholder='Last Name' width={6} onChange={this.handleChange} error={false} />
+                                <Form.Input name='first_name' value={first_name} label='First name' placeholder='First Name' width={6} onChange={this.handleChange} error={false} />
+                                
+                                <Form.Input name='last_name' value={last_name} label='Last Name' placeholder='Last Name' width={6} onChange={this.handleChange} error={false} />
                             </Form.Group>
 
                             <Form.Group>
@@ -117,7 +116,7 @@ class profile extends Component {
 
                             <Form.Group>
                                 <Form.Input name='phone_number' value={phone_number} label='Mobile number' placeholder='+61 0400 000 000' width={6} onChange={this.handleChange} error={false} />
-                                <Form.Input name='website' value={website} label='Website' placeholder='www.mywebsite.com' width={10} onChange={this.handleChange} error={false} />
+                               
                             </Form.Group>
 
                             <Form.Group>
@@ -140,6 +139,9 @@ class profile extends Component {
                     </Grid.Column>
                 </Grid>
             </Segment></>
+            )}
+            </Authenticator>
+
         );
     }
     
