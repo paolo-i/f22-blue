3d7f94cc1e803dbb59d09a651bff08b7d797dd12
Arobe022 <arobe022@odu.edu>
2022-11-27 14:49:01 -0500

    updating the form

--- src/pages/UserArtGalleryPage.js
+++ src/pages/UserArtGalleryPage.js
@@ -1,38 +1,48 @@
 
-import React from "react";
+import React, {Component} from "react";
 import {Authenticator } from '@aws-amplify/ui-react';
-import {Container, Button, Form} from 'react-bootstrap';
 import Amplify from "aws-amplify";
 import {AmplifyS3Album} from "@aws-amplify/ui-react/legacy";
 import Popup from 'reactjs-popup';
+import {Form} from 'semantic-ui-react';
+import axios from 'axios';
 import NavBar from "../custom-components/NavBar";
 import {API} from "aws-amplify"
 import awsExports from "../aws-exports";
 import awsconfig from "../aws-exports";
 
 Amplify.configure(awsconfig);
-async function Submit() {
-  const data = {
-    body: {
-      fileName: formState.fileName,
-      nftAddress: formState.nftAddress,
-      tokenID: formState.tokenID,
-      Whitelist: formState.false
-    }
-  };
-console.log(data);
-const apiData = await API.post('nftapi', '/item', data);
-console.log({ apiData });
-alert('Art Information updated/submitted');
-}
 
-const formState = { fileName: '', nftAddress: '', tokenID: '', Whitelist: false };
+export default class ArtGallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      filename: '',
+      tokenID: '',
+      Whitelist:true
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-function updateFormState(key, value) {
-formState[key] = value;
-
-}
-const ArtGallery= () => {
+  handleChange(event) {
+    const inputValue = event.target.value;
+    const stateField = event.target.name;
+    this.setState({
+      [stateField]: inputValue,
+    });
+    console.log(this.state);
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    const { username, filename, tokenID, Whitelist } = this.state;
+    await axios.post(
+      'https://4937s3ol69.execute-api.us-east-1.amazonaws.com',
+      { key1: `${username}, ${filename},${Whitelist},${tokenID}` }
+    );
+  }
+  render() {
     return (
       <Authenticator>
       {({user }) => (
@@ -42,44 +52,49 @@ const ArtGallery= () => {
     <Popup trigger={<AmplifyS3Album level="private"/>} 
      position="right center">
       <div>
-      <Container>
-    <div>
-      <h3>Artwork Information</h3>
-      <br/>
-        <Form>
+        <h3>Artwork Information</h3>
+        <Form onSubmit={this.handleSubmit}>
+          <label>username:</label>
           <Form.Group>
-            <Form.Label>FileName</Form.Label>
-            <Form.Control placeholder="File Name" onChange={e => updateFormState('fileName', e.target.value)} />
+          <input
+            type="text"
+            placeholder={user.username}
+            name="username"
+            onChange={this.handleChange}
+            value={this.state.username}
+          />
           </Form.Group>
-          <Form.Group>
-            <Form.Label> Have you minted an NFT using this work of art?</Form.Label>
-            <Form.Select  onChange={e => updateFormState('Whitelist', e.target.value)}>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>NFT Address (Leave as is if not whitelisting)</Form.Label>
-            <Form.Control placeholder="N/A" onChange={e => updateFormState('nftAddress', e.target.value)} />
-          </Form.Group>
-         
 
+          <label>File Name:</label>
           <Form.Group>
-            <Form.Label>TokenID (Leave as is if not whitelisting)</Form.Label>
-            <Form.Control placeholder="N/A" onChange={e => updateFormState('tokenID', e.target.value)} />
+          <Form.Input
+            type="text"
+            name="filename"
+            onChange={this.handleChange}
+            value={this.state.filename}
+          />
           </Form.Group>
-          <Button onClick={Submit}>Submit</Button>
+
+          <label> TokenID (Leave as is if not whitelisting):</label>
+          <Form.Group>
+          <input
+            type="text"
+            placeholder="N/A"
+            name="tokenID"
+            onChange={this.handleChange}
+            value={this.state.tokenid}
+          />
+          </Form.Group>
+
+          <button type="submit">Send</button>
         </Form>
-      </div>
-    </Container>
-      </div>
       
+      </div>
     </Popup>
   </div></>
       )}
     </Authenticator>
     
-        )
+        );
     }
-
-  export default ArtGallery
+}
