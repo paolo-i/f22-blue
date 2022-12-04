fffc54a0d7db252826d8d641961387f968b18a3d
Carlo Diaz <carlodiaz139@gmail.com>
2022-12-04 18:08:54 -0500

    Implement DynamoDB notifications table

--- src/pages/NotificationsPage.js
+++ src/pages/NotificationsPage.js
@@ -10,7 +10,6 @@ import { listNotifications } from '../graphql/queries';
 Amplify.configure(awsconfig);
 
 export default function Notifications(props) {
-  /*
   const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
@@ -28,7 +27,6 @@ export default function Notifications(props) {
       console.log('error on fetching notifications', {notifications}, error);
     }
   }
-  */
 
   return (
     <Authenticator>
@@ -36,15 +34,32 @@ export default function Notifications(props) {
         <>
             <NavBar />
               <div class="body">
+                { notifications.map(notification =>{
+                  return (
+                      <Notification 
+                        token_id={notification.id}
+                        blockchain={notification.blockchain}
+                        file={notification.file}
+                        contract_address={notification.contract_address}
+                        img_link={notification.image_url}
+                        marketplace_name={notification.marketplace}
+                        user={notification.user}
+                        />
+                  )
+                })}
+
                 <div class="body">
-                  <Notification />
-                </div>
-                <div class="body">
-                  <Notification />
-                </div>
-                <div class="body">
-                  <Notification />
-                </div>
+                      <Notification 
+                        token_id="7100"
+                        blockchain="ethereum"
+                        file="cryptopunk7597.jpg"
+                        contract_address="0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb"
+                        img_link="https://lh3.googleusercontent.com/SByujrVo6pJzt9dOsdX-OpyMyEX1sjPa_rcdHi-uQ0nBnbVgEve2iKeS-iFvffAPnlt_PtKDeiCdR6yG-quuVO0H2GzvuMnDKso-"
+                        marketplace_name="OpenSea"
+                        user="5af95c2c-828e-42ff-a8a3-48ab2b5c805e"
+                        />
+                    </div>
+                
               </div>
         </>
       )}
